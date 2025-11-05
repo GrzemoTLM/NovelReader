a62ecd537f5325b791ff93e5abbbc2988783dd7e
GrzemoTLM <grzegorz.lukom@gmail.com>
2025-11-05 09:05:57 +0100

    Initialing cilent and server repo's

---
+++ NovelReader_server/src/main/java/org/example/novelreader/NovelReaderApplication.java
@@ -0,0 +1,13 @@
+package org.example.novelreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovelReaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NovelReaderApplication.class, args);
+    }
+
+}
---
+++ NovelReader_server/src/test/java/org/example/novelreader/NovelReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.novelreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovelReaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
